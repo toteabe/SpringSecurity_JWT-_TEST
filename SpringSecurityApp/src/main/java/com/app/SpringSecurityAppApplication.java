a2576f163eb12b75a6bcf157ca686c4ae9d3c943
cgcm070 <cesar.gabriel.martinezs7@gmail.com>
2024-07-25 21:33:17 +0200

    inyectando Usuarios

--- SpringSecurityApp/src/main/java/com/app/SpringSecurityAppApplication.java
+++ SpringSecurityApp/src/main/java/com/app/SpringSecurityAppApplication.java
@@ -3,6 +3,7 @@ package com.app;
 import com.app.persistence.entity.PermisionEntity;
 import com.app.persistence.entity.RoleEntity;
 import com.app.persistence.entity.RoleEnum;
+import com.app.persistence.entity.UserEntity;
 import com.app.persistence.entity.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,48 +16,81 @@ import java.util.Set;
 public class SpringSecurityAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringSecurityAppApplication.class, args);}
-		@Bean
-        CommandLineRunner intit (UserRepository userRepository){
-             return args ->{
-
-                 // Creando Permisos
-                 PermisionEntity createPermission = PermisionEntity.builder()
-                         .name("CREATE").build();
-
-                 PermisionEntity deletePermission = PermisionEntity.builder()
-                         .name("DELETE").build();
-
-                 PermisionEntity updatePermission = PermisionEntity.builder()
-                         .name("UPDATE").build();
-
-                 PermisionEntity readPermission = PermisionEntity.builder()
-                         .name("READ").build();
-
-                 PermisionEntity reafactorPermission = PermisionEntity.builder()
-                         .name("REFACTOR").build();
-
-                 //Create Roles
-                 RoleEntity roleAdmin = RoleEntity.builder().roleEnum(RoleEnum.ADMIN)
-                         .permisionList(Set.of(createPermission,readPermission,deletePermission,updatePermission)).build();
-
-                 RoleEntity roleUser = RoleEntity.builder()
-                         .roleEnum(RoleEnum.USER)
-                         .permisionList(Set.of(createPermission, readPermission))
-                         .build();
-
-                 RoleEntity roleInvited = RoleEntity.builder()
-                         .roleEnum(RoleEnum.INVITED)
-                         .permisionList(Set.of(readPermission))
-                         .build();
-
-                 RoleEntity roleDeveloper = RoleEntity.builder()
-                         .roleEnum(RoleEnum.DEVELOPER)
-                         .permisionList(Set.of(readPermission,createPermission,reafactorPermission,updatePermission,deletePermission))
-                         .build();
-             };
-
-        }
+        SpringApplication.run(SpringSecurityAppApplication.class, args);
     }
+
+    @Bean
+    CommandLineRunner intit(UserRepository userRepository) {
+        return args -> {
+
+            // Creando Permisos
+            PermisionEntity createPermission = PermisionEntity.builder()
+                    .name("CREATE").build();
+
+            PermisionEntity deletePermission = PermisionEntity.builder()
+                    .name("DELETE").build();
+
+            PermisionEntity updatePermission = PermisionEntity.builder()
+                    .name("UPDATE").build();
+
+            PermisionEntity readPermission = PermisionEntity.builder()
+                    .name("READ").build();
+
+            PermisionEntity reafactorPermission = PermisionEntity.builder()
+                    .name("REFACTOR").build();
+
+            //Create Roles
+            RoleEntity roleAdmin = RoleEntity.builder().roleEnum(RoleEnum.ADMIN)
+                    .permisionList(Set.of(createPermission, readPermission, deletePermission, updatePermission)).build();
+
+            RoleEntity roleUser = RoleEntity.builder()
+                    .roleEnum(RoleEnum.USER)
+                    .permisionList(Set.of(createPermission, readPermission))
+                    .build();
+
+            RoleEntity roleInvited = RoleEntity.builder()
+                    .roleEnum(RoleEnum.INVITED)
+                    .permisionList(Set.of(readPermission))
+                    .build();
+
+            RoleEntity roleDeveloper = RoleEntity.builder()
+                    .roleEnum(RoleEnum.DEVELOPER)
+                    .permisionList(Set.of(readPermission, createPermission, reafactorPermission, updatePermission, deletePermission))
+                    .build();
+
+
+            //Creando  Usuarios
+
+            UserEntity userCesar = UserEntity.builder()
+                    .ussername("Cesar")
+                    .password("1234")
+                    .isEnable(true)
+                    .accountNoExpired(true)
+                    .accountNoLocked(true)
+                    .roles(Set.of(roleDeveloper))
+                    .build();
+
+            UserEntity userGabriel = UserEntity.builder()
+                    .ussername("Gabriel")
+                    .password("1234")
+                    .isEnable(true)
+                    .accountNoExpired(true)
+                    .accountNoLocked(true)
+                    .roles(Set.of(roleUser))
+                    .build();
+
+            UserEntity userInvitado = UserEntity.builder()
+                    .ussername("Invitado")
+                    .password("12345")
+                    .isEnable(true)
+                    .accountNoExpired(true)
+                    .accountNoLocked(true)
+                    .roles(Set.of(roleInvited))
+                    .build();
+        };
+
+
+    }
+}
 
 
